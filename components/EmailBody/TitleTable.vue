a6426dc55c5fa10ca242f5ad9e17a89131bf1ab1
Xavier Julien <xjulien@octo.com>
2018-07-30 21:32:19 +0200

    Commit docs site

--- components/EmailBody/TitleTable.vue
+++ components/EmailBody/TitleTable.vue
@@ -3,7 +3,7 @@
         <td bgcolor="#ffffff">
             <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                 <tr>
-                    <td style="padding: 10px; font-family: sans-serif; font-size: 15px; line-height: 140%; color: #555555;">
+                    <td style="padding: 10px; padding-left:0px; font-family: sans-serif; font-size: 15px; line-height: 140%; color: #555555;">
                         <h1
                                 style="margin: 0 0 10px 0; font-family: sans-serif; font-size: 24px; line-height: 125%; color:rgb(20, 160, 192); font-weight: bold;">
                             {{title}}</h1>
